e2a64ac01efee7b024cd7aa4a2059f9aaceeae47
Harrison Harnisch <hharnisc@gmail.com>
2017-02-15 14:15:46 -0600

    more comprehensive render test

--- __tests__/Hoverable.jsx
+++ __tests__/Hoverable.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Hoverable from '../src/Hoverable';
+
+/* eslint-disable react/prop-types */
+
+const TestComponent = ({
+  children,
+  hover,
+  onMouseEnter,
+  onMouseLeave,
+}) => {
+  const style = {
+    background: hover ? 'green' : 'red',
+  };
+  return (
+    <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+/* eslint-enable react/prop-types */
 
 describe('Hoverable', () => {
   it('should render Hoverable', () => {
     const text = 'hi';
-    const hoverable = shallow(
-      <Hoverable><div>{text}</div></Hoverable>,
+    const hoverable = mount(
+      <Hoverable>
+        <TestComponent>{text}</TestComponent>
+      </Hoverable>,
     );
-    expect(hoverable.text())
+    expect(hoverable.find(TestComponent).text())
       .toBe(text);
   });
 });
